7383e700e7fcbb8ff8539596cf865165b6100872
Eugenio Guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-05-17 15:45:20 +0100

    added CosmosDb dependency to C# project version

--- TemperatureSensorC#/IoTproject/Device/device.ino
+++ TemperatureSensorC#/IoTproject/Device/device.ino
@@ -1,47 +1,73 @@
 #include "AZ3166WiFi.h"
 #include "DevKitMQTTClient.h"
+#include "Sensor.h"
 
-static bool hasWifi = false;
-static bool hasIoTHub = false;
+DevI2C *ext_i2c;
+HTS221Sensor *ht_sensor;
+RGB_LED rgbLed;
+
+static float temperatureThreshold = 25.0;
+
+void initSensor() {
+  ext_i2c = new DevI2C(D14, D15);
+  ht_sensor = new HTS221Sensor(*ext_i2c);
+  ht_sensor->init(NULL);
+}
+
+float getSensorData() {
+  float temperature;
+  ht_sensor->enable();
+  ht_sensor->getTemperature(&temperature);
+  ht_sensor->disable();
+  ht_sensor->reset();
+
+  char buff[16];
+  sprintf(buff, "Temp:%sC\r\n", f2s(temperature, 1));
+  Screen.print(1, buff);
+
+  sprintf(buff, "Threshold:%sC\r\n", f2s(temperatureThreshold, 1));
+  Screen.print(2, buff);
+
+  return temperature;
+}
+
+void sendData(const char *data) {
+  time_t t = time(NULL);
+  char buf[sizeof "2011-10-08T07:07:09Z"];
+  strftime(buf, sizeof buf, "%FT%TZ", gmtime(&t));
+
+  EVENT_INSTANCE* message = DevKitMQTTClient_Event_Generate(data, MESSAGE);
+
+  DevKitMQTTClient_Event_AddProp(message, "$$CreationTimeUtc", buf);
+  DevKitMQTTClient_Event_AddProp(message, "$$MessageSchema", "temperature;v1");
+  DevKitMQTTClient_Event_AddProp(message, "$$ContentType", "JSON");
+  
+  DevKitMQTTClient_SendEventInstance(message);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  if (WiFi.begin() == WL_CONNECTED)
-  {
-    hasWifi = true;
-    Screen.print(1, "Running...");
-
-    if (!DevKitMQTTClient_Init())
-    {
-      hasIoTHub = false;
-      return;
-    }
-    hasIoTHub = true;
-  }
-  else
-  {
-    hasWifi = false;
-    Screen.print(1, "No Wi-Fi");
-  }
+  initSensor();
+  WiFi.begin();
+  DevKitMQTTClient_Init(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (hasIoTHub && hasWifi)
-  {
-    char buff[128];
+  float temperature = getSensorData();
 
-    // replace the following line with your data sent to Azure IoTHub
-    snprintf(buff, 128, "{\"topic\":\"iot\"}");
-    
-    if (DevKitMQTTClient_SendEvent(buff))
-    {
-      Screen.print(1, "Sending...");
-    }
-    else
-    {
-      Screen.print(1, "Failure...");
-    }
-    delay(2000);
+  char sensorData[400];
+  sprintf(sensorData,
+          "{\"temperature\":%s,\"threshold\":%s}",
+          f2s(temperature, 1),
+          f2s(temperatureThreshold, 1));
+
+  sendData(sensorData);
+
+  if (temperature > temperatureThreshold) {
+    rgbLed.setColor(255, 0, 0);
   }
+  else {  
+    rgbLed.setColor(0, 0, 255);
+  }
+  
+  delay(5000);
 }
