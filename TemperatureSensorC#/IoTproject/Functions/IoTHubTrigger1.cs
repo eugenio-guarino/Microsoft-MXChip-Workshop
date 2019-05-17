50b12b24a3c07a6e69517982dea80db55e5d49ea
Eugenio Guarino <eugenio.guarino@students.plymouth.ac.uk>
2019-05-17 15:49:47 +0100

    connected function to CosmosDB collection

--- TemperatureSensorC#/IoTproject/Functions/IoTHubTrigger1.cs
+++ TemperatureSensorC#/IoTproject/Functions/IoTHubTrigger1.cs
@@ -6,17 +6,42 @@ using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace IoTWorkbench
 {
-    public static class IoTHubTrigger1
-    {
-        private static HttpClient client = new HttpClient();
+  public class TemperatureItem
+  {
+    [JsonProperty("id")]
+    public string Id {get; set;}
+    public double Temperature {get; set;}
+    public double Threshold {get; set;}
+  }
 
-        [FunctionName("IoTHubTrigger1")]
-        public static void Run([IoTHubTrigger("%eventHubConnectionPath%", Connection = "eventHubConnectionString")]EventData message, ILogger log)
-        {
-            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
-        }
+  public static class IoTHubTrigger1
+  {
+    [FunctionName("IoTHubTrigger1")]
+    public static void Run([IoTHubTrigger("%eventHubConnectionPath%",
+                                          Connection = "eventHubConnectionString")] EventData message,
+                           [CosmosDB(databaseName: "IoTData",
+                                     collectionName: "Temperatures",
+                                     ConnectionStringSetting = "cosmosDBConnectionString")] out TemperatureItem output,
+                            ILogger log)
+    {
+      var deviceId = message.SystemProperties["iothub-connection-device-id"].ToString();
+
+      var jsonBody = Encoding.UTF8.GetString(message.Body);
+      dynamic data = JsonConvert.DeserializeObject(jsonBody);
+      double temperature = data.temperature;
+      double threshold = data.threshold;
+
+      output = new TemperatureItem
+      {
+        Temperature = temperature,
+        Threshold = threshold,
+        Id = deviceId
+      };
     }
+  }
 }
+
